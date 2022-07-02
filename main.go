5249176ba3e7e7d0522610f60439575d17455c30
manimovassagh <mani.mgh@gmail.com>
2022-07-02 23:21:35 +0200

    add modell

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"github.com/thedevsaddam/renderer"
 	mgo "gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
+	"time"
 )
 
 var rnd *renderer.Render
@@ -14,6 +16,15 @@ const (
 	collectionName string = "todos"
 	port           string = ":9000"
 )
+
 //Just to satisfy the compiler
 
-
+type (
+	todoModel struct {
+		ID        bson.ObjectId `bson:"_id,omitempty"`
+		title     string        `bson:"title"`
+		completed bool          `bson:"completed"`
+		createdAt time.Time     `bson:"created_at"`
+	}
+	
+)
