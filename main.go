5325617eeaa5e7db08dd19f542ab06397b0a12c8
manimovassagh <mani.mgh@gmail.com>
2022-07-02 23:47:54 +0200

    add server

--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"log"
+	"net/http"
 	"time"
 
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/thedevsaddam/renderer"
+	"golang.org/x/tools/go/analysis/passes/nilfunc"
 	mgo "gopkg.in/mgo.v2"
 	"gopkg.in/mgo.v2/bson"
 )
@@ -47,7 +51,38 @@ func init() {
 
 func main()  {
 	r:=chi.NewRouter()
+	r.Use(middleware.Logger)
+	r.Get("/",homeHandler)
+	r.Mount("/todo",todoHandlers())
+	srv:=&http.Server{
+		Addr: port,
+		Handler: r,
+		ReadTimeout: 60*time.Second,
+		WriteTimeout: 60*time.Second,
+		IdleTimeout: 60*time.Second,
+
+	}
+	go func ()  {
+		log.Println("Listning to port",port)
+		if err :=srv.ListenAndServe();err!=nil{
+			log.Println("Listen:%s\n",err)
+
+		}
+	}()
 }
+
+
+func todoHandlers() http.Handler {
+	rg:=chi.NewRouter()
+rg.Group(func(r chi.Router) {
+	r.Get("/",fetchTodos)
+	r.Post("/",createTodo)
+	r.Put("/{id}",updateTodo)
+	r.Delete("/{id}",createTodo)
+})
+return rg
+}
+
 
 func checkErr(err error) {
 	if err != nil {
